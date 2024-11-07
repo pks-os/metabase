eaaa4ccd3404a8a0b2d2d23c96c12574d82b9740
Filip Hric <filip@filiphric.sk>
2024-11-07 14:56:32 +0100

    add button to send diagnostic data to slack

--- src/metabase/api/util.clj
+++ src/metabase/api/util.clj
@@ -4,6 +4,7 @@
   (:require
    [cheshire.core :as json]
    [clj-http.client :as http]
+  [clojure.string :as str]
    [compojure.core :refer [GET POST]]
    [crypto.random :as crypto-random]
    [environ.core :refer [env]]
@@ -19,7 +20,13 @@
    [metabase.util.log :as log]
    [metabase.util.malli :as mu]
    [metabase.util.malli.schema :as ms]
-   [ring.util.response :as response]))
+   [ring.util.response :as response])
+  (:import
+   (com.slack.api Slack)
+   (com.slack.api.methods.request.chat ChatPostMessageRequest)
+   (com.slack.api.methods.request.files FilesUploadV2Request FilesSharedPublicURLRequest)))
+
+(set! *warn-on-reflection* true)
 
 (api/defendpoint POST "/password_check"
   "Endpoint that checks if the supplied password meets the currently configured password complexity rules."
@@ -98,5 +105,93 @@
   [:as {{:keys [entity_ids]} :body}]
   {entity_ids :map}
   {:entity_ids (api.embed.common/model->entity-ids->ids entity_ids)})
+
+;; Slack configuration. Files are uploaded to files channel and then a message with action buttons is sent to message channel
+(def ^:private slack-bot-token (env :mb-slack-bot-token))
+(def ^:private slack-files-channel-id "C07PK7KSAUV")
+(def ^:private slack-message-channel-id "C07MSLAUVK2")
+
+(defn- create-slack-message-blocks [diagnostic-info ^com.slack.api.model.File file-info]
+  (let [metabase-info (get-in diagnostic-info [:bugReportDetails :metabase-info])
+        system-info (get-in diagnostic-info [:bugReportDetails :system-info])
+        version-info (get-in diagnostic-info [:bugReportDetails :metabase-info :version])]
+    (json/generate-string
+     [{:type "section"
+       :text {:type "mrkdwn"
+              :text "A new bug report has been submitted. Please check it out!"}}
+      {:type "section"
+       :fields [{:type "mrkdwn"
+                 :text (str "*URL:*\n" (get diagnostic-info :url "N/A"))}
+                {:type "mrkdwn"
+                 :text (str "*App database:*\n"
+                          (get metabase-info :application-database "N/A"))}
+                {:type "mrkdwn"
+                 :text (str "*Java Runtime:*\n"
+                          (get system-info :java.runtime.name "N/A"))}
+                {:type "mrkdwn"
+                 :text (str "*Java Version:*\n"
+                          (get system-info :java.runtime.version "N/A"))}
+                {:type "mrkdwn"
+                 :text (str "*OS Name:*\n"
+                          (get system-info :os.name "N/A"))}
+                {:type "mrkdwn"
+                 :text (str "*OS Version:*\n"
+                          (get system-info :os.version "N/A"))}
+                {:type "mrkdwn"
+                 :text (str "*Version info:*\n```"
+                          (json/generate-string version-info {:pretty true})
+                          "```")}]}
+      {:type "divider"}
+      {:type "actions"
+       :elements [{:type "button"
+                  :text {:type "plain_text"
+                        :text "Jump to debugger"
+                        :emoji true}
+                  :url (str "https://metabase-debugger.vercel.app/?fileId="
+                           (str/replace (.getId ^com.slack.api.model.File file-info) "\"" ""))
+                  :style "primary"}
+                 {:type "button"
+                  :text {:type "plain_text"
+                        :text "Download the report"
+                        :emoji true}
+                  :url (.getUrlPrivateDownload ^com.slack.api.model.File file-info)}]}])))
+
+(api/defendpoint POST "/send-to-slack"
+  "Send diagnostic information to a Slack channel using the Slack API."
+  [:as {{:keys [diagnosticInfo]} :body}]
+  {diagnosticInfo [:maybe :map]}
+  (try
+    (let [slack (Slack/getInstance)
+          methods (.methods slack)
+          file-upload-response (.filesUploadV2 methods
+                               (.. (FilesUploadV2Request/builder)
+                                   (token slack-bot-token)
+                                   (channel slack-files-channel-id)
+                                   (content (json/generate-string diagnosticInfo {:pretty true}))
+                                   (filename "diagnostic-info.json")
+                                   (requestFileInfo true)
+                                   build))
+          ^com.slack.api.model.File file (first (.getFiles file-upload-response))]
+      (when (.isOk file-upload-response)
+        (.filesSharedPublicURL methods
+         (.. (FilesSharedPublicURLRequest/builder)
+             (token slack-bot-token)
+             (file (.getId ^com.slack.api.model.File file))
+             build))
+        (.chatPostMessage methods
+         (.. (ChatPostMessageRequest/builder)
+             (token slack-bot-token)
+             (channel slack-message-channel-id)
+             (text "A new bug report has been submitted.")
+             (blocksAsString (create-slack-message-blocks diagnosticInfo file))
+             build)))
+      {:status 200
+       :body {:success true
+              :file-url (.getPermalinkPublic ^com.slack.api.model.File file)}})
+    (catch Exception e
+      (log/error e "Error sending to Slack")
+      {:status 500
+       :body {:success false
+              :message (.getMessage e)}})))
 
 (api/define-routes)
